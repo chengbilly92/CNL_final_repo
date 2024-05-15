f0c43116d09cfdbe185729fde6a954177f37dcdb
jcplist <91445595+jcplist@users.noreply.github.com>
2024-05-15 21:58:10 +0800

    add exception handling

--- pages/login.py
+++ pages/login.py
@@ -1,16 +1,24 @@
 import streamlit as st
 import sqlite3
+import traceback
 
 def checkLogin(username: str, password: str)-> None:
-    con = sqlite3.Connection('user.db')
-    cur: sqlite3.Cursor = con.cursor()
-    res = cur.execute('SELECT password FROM users WHERE username = ?;', (username,))
+    try:
+        con = sqlite3.Connection('user.db')
+        cur: sqlite3.Cursor = con.cursor()
+        res = cur.execute('SELECT password FROM users WHERE username = ?;', (username,)).fetchone()
 
-    if password == res.fetchone()[0]:
-        st.success("Login successful!")
-        st.session_state['login'] = username
-    elif username != "" or password != "":
+        if res and password and password == res[0]:
+            st.success("Login successful!")
+            st.session_state['login'] = username
+        else:
+            st.error("Invalid username or password.")
+    except:
+        traceback.print_exc()
         st.error("Invalid username or password.")
+    finally:
+        con.close()
+
 
 def main():
     st.title("Simple Login Page")
