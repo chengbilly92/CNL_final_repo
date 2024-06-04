b04cceccb42c35f8794ddaba7798a459058ae019
Lin Howard <b10902004@ntu.edu.tw>
2024-06-04 22:42:40 +0800

    add change man_language

--- pages/changeLanguage.py
+++ pages/changeLanguage.py
@@ -5,9 +5,19 @@ def main() -> None:
     utils.set_sidebar()
     st.title('Change password')
     language = st.radio("",[
-        "auto", ":flag-us: English", ":flag-jp: 日本語", ":flag-tw: 繁體中文", ":flag-fr: Français"
+        "auto", ":flag-us: English", ":flag-jp: 日本語", 
+        ":flag-tw: 繁體中文",
     ])
-    st.session_state['man_language'] = language
+    match language:
+        case "auto":
+            st.session_state['man_language'] = "unknown"
+        case ":flag-us: English":
+            st.session_state['man_language'] = "unknown"
+        case ":flag-jp: 日本語":
+            st.session_state['man_language'] = "Japan"
+        case ":flag-tw: 繁體中文":
+            st.session_state['man_language'] = "Taiwan"
+
 
 if __name__ == "__main__":
     main()
