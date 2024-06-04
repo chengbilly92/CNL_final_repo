b4a490c965510650f4983921debbf8e4fa063bbb
Lin Howard <b10902004@ntu.edu.tw>
2024-06-05 05:01:24 +0800

    last time

--- index.py
+++ index.py
@@ -56,7 +56,7 @@ def get_ip_info(user_ip: str)-> dict:
 
 def main()-> None:
     if "country" in st.session_state:
-        del st.session_state["country"]
+        st.session_state["country"] = "unknown"
     user_ip: str = get_forwarded_ip()
     user_ip_info: dict = get_ip_info(user_ip)
     st.session_state['userip'] = "localhost" if user_ip_info['status'] == "fail" else user_ip
