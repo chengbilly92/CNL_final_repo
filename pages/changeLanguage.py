adf63ac9df6f4126ec70f3ac99eb2d82b032fb65
Lin Howard <b10902004@ntu.edu.tw>
2024-06-05 03:43:52 +0800

    Bugfix language changing

--- pages/changeLanguage.py
+++ pages/changeLanguage.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import utils
 
+def on_change() -> None:
+    st.session_state['clanguage'] = True
+
 def main() -> None:
     utils.set_sidebar()
     st.title(utils.ChangeLanguage[st.session_state['country']])
     language = st.radio("",[
         "auto", ":flag-us: English", ":flag-jp: 日本語", 
         ":flag-tw: 繁體中文",
-    ])
+    ], on_change= on_change)
     match language:
         case "auto":
             st.session_state["country"] = "unknown"
@@ -18,7 +21,9 @@ def main() -> None:
         case ":flag-tw: 繁體中文":
             st.session_state["country"] = "Taiwan"
 
-    st.switch_page('index.py')
+    if 'clanguage' in st.session_state:
+        del st.session_state['clanguage']
+        st.rerun()
 
 if __name__ == "__main__":
     main()
