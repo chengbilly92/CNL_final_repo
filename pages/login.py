cac12731794394e7c1c3308e5f83030d5cfc9f4b
Lin Howard <b10902004@ntu.edu.tw>
2024-06-04 20:03:59 +0800

    fix form submit

--- pages/login.py
+++ pages/login.py
@@ -2,6 +2,7 @@ import streamlit as st
 import sqlite3
 import traceback
 import utils
+import time
 
 def check_login(username: str, password: str)-> None:
     try:
@@ -22,14 +23,31 @@ def check_login(username: str, password: str)-> None:
 
 def main():
     utils.set_sidebar()
-    if 'login' in st.session_state:
-        st.switch_page('index.py')
     st.title("Simple Login Page")
 
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+    with st.form('loginform'):
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        submited = st.form_submit_button('Login')
+        if submited:
+            try:
+                con = sqlite3.Connection('user.db')
+                cur: sqlite3.Cursor = con.cursor()
+                res = cur.execute('SELECT password FROM users WHERE username = ?;', (username,)).fetchone()
+                if res and password and password == res[0]:
+                    st.success("Login successful!")
+                    st.session_state['login'] = username
+                else:
+                    st.error("Invalid username or password.")
+            except:
+                traceback.print_exc()
+                st.error("Invalid username or password.")
+            finally:
+                con.close()
 
-    st.button('Login', on_click=lambda: check_login(username, password))
+    if 'login' in st.session_state:
+        time.sleep(0.5)
+        st.switch_page('index.py')
 
 if __name__ == "__main__":
     main()
